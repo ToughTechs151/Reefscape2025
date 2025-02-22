18dab3453fec26a660fe980e710f41c20be37afe
Tejas1551 <994806404@nsd42.net>
2025-02-22 18:04:23 -0500

    Load Coral Command Fixes #51

--- src/main/java/frc/robot/subsystems/RollerSubsystem.java
+++ src/main/java/frc/robot/subsystems/RollerSubsystem.java
@@ -20,7 +20,9 @@ import edu.wpi.first.wpilibj.DataLogManager;
 import edu.wpi.first.wpilibj.RobotBase;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.FunctionalCommand;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 import frc.robot.Constants.RollerConstants;
@@ -251,14 +253,12 @@ public class RollerSubsystem extends SubsystemBase implements AutoCloseable {
         this);
   }
 
-  /** Returns a Command that runs the motor forward until a coral is loaded. */
+  /** Loads Coral until CANRange detects Coral and then runs for a short time after */
   public Command loadCoral() {
-    return new FunctionalCommand(
-        this::setMotorSetPointForward,
-        this::updateMotorController,
-        interrupted -> disableRoller(),
-        this::isCoralInsideRoller,
-        this);
+    return Commands.sequence(
+        runReverse().until(this::isCoralInsideRoller),
+        runReverse().withTimeout(0.25),
+        new InstantCommand(this::disableRoller));
   }
 
   /** Returns a Command that runs the motor in reverse at the current set speed. */
